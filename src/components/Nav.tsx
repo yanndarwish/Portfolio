0e14d4bc4c4dc88d7401abb341783ced8cad1745
Yann Darwish <yann.darwish@gmail.com>
2023-04-02 21:01:46 +0200

    z index fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ import { BsClipboardData, BsBriefcase, BsChatSquareText } from "react-icons/bs"
 
 const Nav: FC = () => {
 	return (
-		<nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50">
+		<nav className="fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-[99]">
 			<div className="container mx-auto">
 				{/* nav inner */}
 				<div className="w-fill bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between items-center text-2xl text-white/50">
