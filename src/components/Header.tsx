e9ac13dfaab235988df4107856566f3daa604537
Yann Darwish <yann.darwish@gmail.com>
2023-04-02 20:47:54 +0200

    header

---
+++ src/components/Header.tsx
@@ -0,0 +1,38 @@
+// FC
+import { FC } from "react"
+// react-scroll
+import { Link } from "react-scroll"
+// react router
+import { useLocation } from "react-router-dom"
+
+const Header: FC = () => {
+	const location = useLocation()
+
+	console.log(location)
+
+	return (
+		<header className="py-8">
+			<div className="container mx-auto">
+				<div className="flex justify-between items-center">
+					{/* logo */}
+					<a href="/">
+						<h3 className="text-gradient h3 mb-0">Yann</h3>
+						<h3 className="h3">Darwish</h3>
+					</a>
+					{/* button */}
+					{location.pathname === "/" ? (
+						<Link to="work" smooth={true}>
+							<button className="btn btn-sm">See my work</button>
+						</Link>
+					) : (
+						<a href="/projects">
+							<button className="btn btn-sm">My projects</button>
+						</a>
+					)}
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Header
--- src/router/Root.tsx
+++ src/router/Root.tsx
@@ -3,12 +3,12 @@ import { FC } from "react"
 // Outlet
 import { Outlet } from "react-router-dom"
 // components
-// import Header from "../components/Header"
+import Header from "../components/Header"
 
 const Root: FC = () => {
 	return (
 		<div className="bg-site bg-cover bg-no-repeat overflow-hidden app">
-			{/* <Header /> */}
+			<Header />
 			<Outlet />
 		</div>
 	)
